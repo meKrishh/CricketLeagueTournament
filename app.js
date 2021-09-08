a3fb5ee3aa915d0c6cb38893b14976d43a20e55d
meKrishh <48477839+meKrishh@users.noreply.github.com>
2021-09-08 23:58:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // express app
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const Player = require('./Models/Player');
 const Matches = require('./Models/Matches');
